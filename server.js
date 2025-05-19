0a988e70c49f8675e4ccf89c547a6ec3a02d4786
LakshmiChe <14uece050@gmail.com>
2025-05-19 04:29:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -18,3 +18,10 @@ app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const path = require('path');
+
+// Serve static HTML page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
